02e71f8237cba3bd03eac949e48bd7cfcb9f3216
Jesper Berg <jesper.berg@cygni.se>
2021-12-02 09:11:13 +0100

    Day two completed. Not yet refactored

--- day02/index.js
+++ day02/index.js
@@ -1,9 +1,44 @@
+const fs = require('fs');
+const file = fs.readFileSync('./input.txt').toString().split("\n").map(str => str.split(' '));
+
 function getSolutionPart1() {
-    return 1337;
+    depth = 0;
+    pos = 0;
+    file.forEach(ins => {
+        value = parseInt(ins[1]);
+        switch(ins[0]) {
+            case 'forward':
+                pos += value;
+                break;
+            case 'down':
+                depth += value;
+                break;
+            case 'up':
+                depth -= value;
+        }
+    })
+    return pos * depth;
 }
 
 function getSolutionPart2() {
-    return 42;
+    depth = 0;
+    pos = 0;
+    aim = 0;
+    file.forEach(ins => {
+        value = parseInt(ins[1]);
+        switch(ins[0]) {
+            case 'forward':
+                pos += value;
+                depth += value * aim;
+                break;
+            case 'down':
+                aim += value;
+                break;
+            case 'up':
+                aim -= value;
+        }
+    })
+    return pos * depth;
 }
 
 const part = process.env.part || "part1";
