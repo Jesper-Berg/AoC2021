d860d1b7ed2a6b8f320de4287a90c1d6130894c0
Jesper Berg <jesper.berg@cygni.se>
2021-12-03 11:46:00 +0100

    day 3, I hate promises and async await

--- day03/index.js
+++ day03/index.js
@@ -1,9 +1,24 @@
+const fs = require('fs');
+const file = fs.readFileSync('./input.txt').toString().split("\n");
+
 function getSolutionPart1() {
-    return 1337;
+    sum = new Array(file[0].length).fill(0);
+    file.forEach(binStr => binStr.split('').map((val, i) => sum[i] += parseInt(val)));
+    gammaBin = sum.map(total => (total > file.length / 2) ? 1 : 0).join('');
+    epsilonBin = gammaBin.split('').map(num => parseInt(num) ? 0 : 1).join('');
+    return parseInt(gammaBin, 2) * parseInt(epsilonBin, 2);
 }
 
-function getSolutionPart2() {
-    return 42;
+const findRating = async (list, index, mostFrequent) => {
+    if(list.length === 1) return list[0];
+    sum = list.filter(val => parseInt(val[index])).length;
+    desNum = !(sum >= list.length / 2 ? !mostFrequent : mostFrequent) ? 1 : 0; //NOT XOR
+    return await findRating(list.filter(val => parseInt(val[index]) === desNum), index + 1, mostFrequent);
+}
+
+function getSolutionPart2 () {
+    return Promise.all([findRating(file, 0, true), findRating(file, 0, false)])
+                .then(values => values.map(val => parseInt(val, 2)).reduce((prev, curr) => prev * curr))
 }
 
 const part = process.env.part || "part1";
@@ -11,4 +26,4 @@ const part = process.env.part || "part1";
 if (part === "part1")
     console.log(getSolutionPart1());
 else
-    console.log(getSolutionPart2());
+    getSolutionPart2().then(value => console.log(value));
